6e6c95b69b94a629aa1b932f60f6241b878e076a
xiaoliangL <mango_liang@outlook.com>
2020-02-24 20:46:28 +0800

    尝试传参搜索

--- shanxi-firstweb-web/src/main/java/com/kfgs/firstweb/controller/SimulationWebController.java
+++ shanxi-firstweb-web/src/main/java/com/kfgs/firstweb/controller/SimulationWebController.java
@@ -1,15 +1,16 @@
 package com.kfgs.firstweb.controller;
 
-import okhttp3.Headers;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import com.alibaba.fastjson.JSON;
+import com.kfgs.domain.ext.FormDate;
+import okhttp3.*;
 import org.apache.http.cookie.SetCookie;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
@@ -24,10 +25,11 @@ import java.util.Map;
  * @author : mango
  * @date : 2020/2/15 15:03
  */
-@RestController
+@Controller
 @RequestMapping("/Simulation")
 public class SimulationWebController {
 
+    @ResponseBody
     @GetMapping("/getCookie")
     public Map getCookie(HttpServletRequest servletRequestrequest) throws IOException {
         String userAgent = servletRequestrequest.getHeader("User-Agent");
@@ -55,4 +57,37 @@ public class SimulationWebController {
         result.put("session",jession);
         return result;
     }
+
+    @PostMapping("/getResponseBody")
+    public void getResponseBody(HttpServletRequest req, HttpServletResponse resp, String url, @RequestBody FormDate formDate) throws IOException {
+        System.out.println(req);
+        System.out.println(req.getRequestedSessionId());
+        System.out.println(req.getRequestedSessionId().replaceAll("%22",""));
+        String formDateJson = JSON.toJSONString(formDate);
+        //请求body
+        okhttp3.RequestBody body;
+        body = okhttp3.RequestBody.create(MediaType.parse("application/x-www-form-urlencoded"), formDateJson);
+        System.out.println(body);
+        String userAgent = req.getHeader("User-Agent");
+        OkHttpClient client=new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(url)
+                .addHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+                .addHeader("Connection","keep-alive")
+                .addHeader("Accept-Language", "zh-CN,zh;q=0.9")
+                .addHeader("Host","www.patviewer.com")
+                .addHeader("User-Agent",userAgent)
+                .addHeader("Cookie",req.getRequestedSessionId().replaceAll("%22",""))
+                //.addHeader("User-Agent",req.getHeader())
+                .post( body)
+                .build();
+
+        okhttp3.Call call = client.newCall(request);
+        // 执行GET请求，并在异步回调中处理请求
+        Response response = call.execute();
+
+        System.out.println(formDate.toString());
+    }
+
+
 }
--- shanxi-firstweb-web/src/main/webapp/js/service/indexService.js
+++ shanxi-firstweb-web/src/main/webapp/js/service/indexService.js
@@ -4,4 +4,9 @@ app.service('indexService',function ($http) {
     this.getPatviewerCookie = function () {
         return $http.get("../../Simulation/getCookie.do");
     }
+
+    this.searchQuest = function (url,formDate) {
+        console.log(formDate)
+        return $http.post("../../Simulation/getResponseBody.do?url=" + url, formDate );
+    }
 });
