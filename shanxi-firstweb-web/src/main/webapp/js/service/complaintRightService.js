d71e9aa43f253088ecef1727cd3b03944f7b2f4b
yuhui0919 <1150066168@qq.com>
2020-03-25 15:08:43 +0800

    投诉详情

---
+++ shanxi-firstweb-web/src/main/webapp/js/service/complaintRightService.js
@@ -0,0 +1,9 @@
+//统计service层
+app.service('complaintRightService',function ($http) {
+    //列表页加载
+    this.load = function (searchMap) {
+        return $http.post('../../product/getList.do',searchMap);
+    }
+
+
+});
